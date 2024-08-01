eb2388ea6256ba46dcfc1a2269ee5d2207bea238
gustavdlima <gustavdlima@gmail.com>
2024-07-31 23:07:27 -0300

    chore: test to check the struct and enum created

--- src/roman_converter.c
+++ src/roman_converter.c
@@ -2,6 +2,26 @@
 
 int	main(int argc, char **argv)
 {
-	printf("size of argc: %d\n", argc);
-	printf("your argument is: %s\n", argv[1]);
+	printf("%i\n", argc);
+	printf("%s\n\n\n\n\n", argv[1]);
+	printf("%d\n", TOTAL_ROMAN_NUMERALS);
+
+	RomanNumeralMap romanNumerals[TOTAL_ROMAN_NUMERALS] = {
+		{M, 1000, "M"},
+		{CM, 900, "CM"},
+		{D, 500, "D"},
+		{CD, 400, "CD"},
+		{C, 100, "C"},
+		{XC, 90, "XC"},
+		{L, 50, "L"},
+		{XL, 40, "XL"},
+		{X, 10, "X"},
+		{IX, 9, "IX"},
+		{V, 5, "V"},
+		{IV, 4, "IV"},
+		{I, 1, "I"},
+	};
+
+	printf("%s\n", romanNumerals[0].symbol);
+
 }
