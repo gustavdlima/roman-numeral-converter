22d911bee682115573b61a671d2d329dcb332e31
gustavdlima <gustavdlima@gmail.com>
2024-08-04 19:13:02 -0300

    chore: struct and enum are now in main.h

---
+++ includes/main.h
@@ -0,0 +1,38 @@
+#ifndef main_H
+#define main_H
+
+typedef enum {
+	M,
+	CM,
+	D,
+	CD,
+	C,
+	XC,
+	L,
+	XL,
+	X,
+	IX,
+	V,
+	IV,
+	I,
+} RomanNumeral;
+
+typedef struct {
+	RomanNumeral roman_numeral;
+	int value;
+	char *symbol;
+} RomanNumeralMap;
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "validations.h"
+#include "utils.h"
+#include "convert.h"
+
+#define TOTAL_ROMAN_NUMERALS 13
+#define MAX_ROMAN_NUMERAL_LENGTH 3999
+
+
+
+#endif
--- src/main.c
+++ src/main.c
@@ -1,4 +1,4 @@
-#include "roman_converter.h"
+#include "main.h"
 
 int	main(int argc, char **argv)
 {
