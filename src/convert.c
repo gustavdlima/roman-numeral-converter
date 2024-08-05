a61c25412813ea06680e2a57f6f73f15adce501d
gustavdlima <gustavdlima@gmail.com>
2024-08-04 21:55:13 -0300

    fix: add validation to romanNumeralString malloc
    fix: add temp variable to handle better the memory management

--- src/convert.c
+++ src/convert.c
@@ -1,42 +1,49 @@
 #include "main.h"
 
-int convertRomanNumeralsToInteger(char *romanNumeralString, RomanNumeralMap *romanNumerals) {
+int convertRomanToInteger(char *romanNumeralString) {
 	int result = 0;
 	int lastValue = 0;
 
 	for (int i = strlen(romanNumeralString) - 1; i >= 0; i--) {
-		int actualValue = returnRomanNumeralIntegerValue(romanNumeralString[i], romanNumerals);
+		int actualValue = returnRomanNumeralIntegerValue(romanNumeralString[i]);
 
-		if (actualValue <= lastValue)
+		if (actualValue < lastValue)
 			result -= actualValue;
 		else
 			result += actualValue;
 
 		lastValue = actualValue;
 	}
-
 	return result;
 }
 
 char *convertIntegerToRomanNumerals(int number, RomanNumeralMap *romanNumerals)
 {
-	char *romanNumeralString = (char *)malloc(sizeof(char) * 1);
+	char *romanNumeralString = (char *)malloc(1);
 
-	for (int i = 0; i < TOTAL_ROMAN_NUMERALS; i++)
-	{
-		while (number >= romanNumerals[i].value)
-		{
-			// dynamic allocation of memory for romanNumeralString
+	if (romanNumeralString == NULL) {
+		printf("Memory allocation failed\n");
+		exit(1);
+	}
+
+	romanNumeralString[0] = '\0';
+
+	for (int i = 0; i < TOTAL_ROMAN_NUMERALS; i++) {
+		while (number >= romanNumerals[i].value) {
 			size_t newSize = strlen(romanNumeralString) + strlen(romanNumerals[i].symbol) + 1;
-			romanNumeralString = (char *)realloc(romanNumeralString, newSize);
-			if (romanNumeralString == NULL)
-			{
+			char *temp = (char *)realloc(romanNumeralString, newSize);
+
+			if (temp == NULL) {
 				printf("Memory allocation failed\n");
+				free(romanNumeralString);
 				exit(1);
 			}
+
+			romanNumeralString = temp;
 			strcat(romanNumeralString, romanNumerals[i].symbol);
 			number -= romanNumerals[i].value;
 		}
 	}
+	free(romanNumerals);
 	return romanNumeralString;
 }
