d58834f46c6de30c54b44cd93809e9e68e954e46
gustavdlima <gustavdlima@gmail.com>
2024-08-04 21:59:45 -0300

    feat: add new struct to handle with Unique Roman Numerals

--- includes/main.h
+++ includes/main.h
@@ -20,15 +20,23 @@ typedef enum {
 typedef struct {
 	RomanNumeral roman_numeral;
 	int value;
+	char symbol;
+} UniqueRomanNumeralMap;
+
+typedef struct {
+	RomanNumeral roman_numeral;
+	int value;
 	char *symbol;
 } RomanNumeralMap;
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 #include "validations.h"
 #include "utils.h"
 #include "convert.h"
+#include "cli.h"
 
 #define TOTAL_ROMAN_NUMERALS 13
 #define MAX_ROMAN_NUMERAL_LENGTH 3999
